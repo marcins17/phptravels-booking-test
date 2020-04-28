31f95449e2071e768afa7d957eb7e5074c5fcea3
marcins17 <marcin.skop17@gmail.com>
2020-04-28 18:32:13 +0200

    Travel points creation moved to set_travel_point() function

--- features/step_definitions/phptravels_booking_test_steps.rb
+++ features/step_definitions/phptravels_booking_test_steps.rb
@@ -1,5 +1,6 @@
 require "cucumber"
 require "watir"
+require_relative "custom_methods.rb"
 
 Given(/^I am on the PHPTRAVELS booking site/) do
   @browser.goto "https://www.phptravels.net/home"
@@ -10,43 +11,16 @@ When(/^I select "([^"]*)" section$/) do |arg|
 end
 
 And(/^I provide "([^"]*)" as a starting point$/) do |arg|
-  result_label = @browser.div(:class => "select2-result-label")
   @browser.div(:id => "s2id_location_from").click
-  if @browser.text_field(:class => "select2-input").exists?
-    @browser.text_field(:class => /select2-focused/).set("#{arg}")
-    if result_label.exists?
-      result_label.click
-    else
-      @browser.div(:id => "select2-drop-mask").click
-    end
-  end
+  set_travel_point("#{arg}")
 end
 
 And(/^I provide "([^"]*)" as a destination point$/) do |arg|
-  #result_li = @browser.li(:class => /result-selectable/)
   @browser.div(:id => "s2id_location_to").click
-  if @browser.text_field(:class => "select2-input").exists?
-    @browser.text_field(:class => /select2-focused/).set("#{arg}")
-    # if result_li.exists?
-    #   @browser.text_field(:class => /select2-focused/).click
-    # else
-    #   @browser.div(:id => "select2-drop-mask").click
-    # end
-  end
-  sleep(3)
-  @browser.div(:class => "select2-result-label").click
+  set_travel_point("#{arg}")
 end
 
 Then(/^flight results should be displayed$/) do
   sleep(2)
 
 end
-
-# def provide_travel_point(location_div, city)
-#   @browser.div(:id => "#{location_div}").click
-#   if @browser.text_field(:class => "select2-input").exists?
-#     @browser.text_field(:class => /select2-focused/).set("#{city}")
-#     sleep(3)
-#     @browser.div(:class => "select2-result-label").click
-#   end
-# end
