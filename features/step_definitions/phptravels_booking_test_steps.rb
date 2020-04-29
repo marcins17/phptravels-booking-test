28d99bfe9aac9d06e2f41ff951beba2b52f2ab8f
marcins17 <marcin.skop17@gmail.com>
2020-04-29 21:06:02 +0200

    Steps implemented: departure date, search button

--- features/step_definitions/phptravels_booking_test_steps.rb
+++ features/step_definitions/phptravels_booking_test_steps.rb
@@ -1,8 +1,7 @@
-require 'cucumber'
-require 'watir'
 require_relative 'custom_methods.rb'
+require 'date'
 
-Given(/^I am on the PHPTRAVELS booking site/) do
+Given(/^I am on the booking site homepage/) do
   @browser.goto "https://www.phptravels.net/home"
 end
 
@@ -12,53 +11,39 @@ end
 
 And(/^I provide "([^"]*)" as a starting point$/) do |arg|
   @browser.div(:id => "s2id_location_from").click
-  puts "Inserting #{arg} as a starting point"
   set_travel_point("#{arg}")
 end
 
 And(/^I provide "([^"]*)" as a destination point$/) do |arg|
   @browser.div(:id => "s2id_location_to").click
-  puts "Inserting #{arg} as a destination point"
   set_travel_point("#{arg}")
 end
 
-And(/^I select depart date two weeks later than current date$/) do
-  #depart_date_list = format_departure_date
-  depart_date_list = [2020, 3, 29]
-  puts depart_date_list
+And(/^I select departure date in "([^"]*)" days$/) do |arg|
+  current_date = Date.today
+  depart_date_list = get_date_and_return_departure_date(current_date, ("#{arg}").to_i)
+  #depart_date_list = [2020, 3, 29]
+  call_calendar = @browser.input(:name => "departure_date")
+  call_calendar.click
+  departure_date_input = @browser.div(:class => %w(datepicker active)).div(:data_date => depart_date_list[2].to_s,
+                                                                           :data_month => depart_date_list[1].to_s,
+                                                                           :data_year => depart_date_list[0].to_s)
+  next_month_button = @browser.div(:class => %w(datepicker active)).div(:data_action => "next")
+  insert_departure_date(departure_date_input, next_month_button)
+end
 
-  @browser.input(:name => "departure_date").click
-  sleep(5)
-  depart_day_button = @browser.div(:data_date => depart_date_list[2].to_s,
-                                    :data_month => depart_date_list[1].to_s,
-                                    :data_year => depart_date_list[0].to_s)
-  puts "depart_day_button"
-  puts depart_day_button.exists?
-  puts depart_day_button.visible?
-  puts depart_day_button.present?
+And(/^I select "([^"]*)" adults$/) do |arg|
+  set_number_of_passenger("#{arg}".to_i)
+end
 
-  sleep(5)
-  next_month_nav_button = @browser.div(:data_action => "next", :class => "datepicker--nav-action")
-  puts "next_month_nav_button"
-  puts next_month_nav_button.exists?
-  puts next_month_nav_button.visible?
-  puts next_month_nav_button.present?
+And(/^I select "([^"]*)" children$/) do |arg|
+  set_number_of_passenger("#{arg}".to_i)
+end
 
-
-
-  # @browser.div(:data_date => depart_date_list[2].to_s,
-  #              :data_month => depart_date_list[1].to_s,
-  #              :data_year => depart_date_list[0].to_s).click
-
-  #puts @browser.div(:data_action => "next", :class => "datepicker--nav-action").exists?
-  #@browser.div(:data_action => "next", :class => "datepicker--nav-action").click
-
-  # puts @browser.element(:tag_name => "svg").exists? #:class => "datepicker--nav-action",
-  # @browser.button(:tag_name => "svg").click #:class => "datepicker--nav-action",
-  # sleep(2)
-  # puts @browser.div(:data_date => day_formatted,
-  #                   :data_month => month_formatted_decremented.to_s,
-  #                   :data_year => year).exists?
+And(/^I submit data by clicking search button$/) do
+  search_button = @browser.button(:type => "submit", :class => "btn-primary btn btn-block")
+  search_button.fire_event("mousedown")
+  search_button.fire_event("click")
 end
 
 Then(/^flight results should be displayed$/) do
